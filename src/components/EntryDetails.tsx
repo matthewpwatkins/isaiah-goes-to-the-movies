e529cd10451650bec3cb3de849c4de686da95c6d
Matthew Watkins <3808901+TodoCleverNameHere@users.noreply.github.com>
2023-10-24 20:54:45 -0500

    Yaml Parsing working well

--- src/components/EntryDetails.tsx
+++ src/components/EntryDetails.tsx
@@ -1,15 +1,23 @@
 import { useEffect, useState } from "react";
 import { Card } from "react-bootstrap";
 import { useParams } from "react-router-dom";
+import { parse } from "yaml";
+import Entry from "../models/Entry";
 
 function EntryDetails() {
   const { id } = useParams();
-  const [entry, setEntry] = useState<string>("");
+  const [entry, setEntry] = useState<Entry | undefined>(undefined);
 
   async function loadEntry() {
-    const res = await fetch(`/api/${id}`);
+    const res = await fetch(`/api/${id!}`);
     const entryText = await res.text();
-    setEntry(entryText);
+    const frontMatterStartIndex = entryText.indexOf('---');
+    const frontMatterEndIndex = entryText.lastIndexOf('---');
+    const frontMatter = entryText.substring(frontMatterStartIndex + 3, frontMatterEndIndex).trim();
+    const entry = parse(frontMatter) as Entry;
+    entry.id = id!;
+    entry.contentMarkdown = entryText.substring(frontMatterEndIndex + 3).trim();
+    setEntry(entry);
   }
 
   useEffect(() => {
@@ -17,7 +25,7 @@ function EntryDetails() {
   }, [id]);
 
   return <Card border="secondary" className="p-3 mb-5">
-    <pre>{entry}</pre>
+    <pre>{JSON.stringify(entry, undefined, '  ')}</pre>
   </Card>;
 }
 
--- src/models/Entry.ts
+++ src/models/Entry.ts
@@ -1,4 +1,6 @@
 export default interface Entry {
   id: string;
+  title: string;
   visited: boolean;
+  contentMarkdown: string;
 };
