f761db6b283c5bbca548c32e0ddf514dc3b5db85
Matthew Watkins <3808901+TodoCleverNameHere@users.noreply.github.com>
2023-11-06 14:49:27 -0600

    Use test flag in GA

--- src/components/Analytics.tsx
+++ src/components/Analytics.tsx
@@ -1,28 +1,29 @@
 import ReactGA from "react-ga4";
 
-let isProd : boolean | undefined = undefined;
-let previousPath : string | undefined = undefined;
+let isProd: boolean | undefined = undefined;
+let previousPath: string | undefined = undefined;
 
 function checkEnvironment() {
   if (isProd === undefined) {
-    isProd = window.location.hostname.endsWith('.com');
+    isProd = window.location.hostname.endsWith(".com");
   }
 }
 
 export function initAnalytics() {
   checkEnvironment();
-  if (isProd) {
-    ReactGA.initialize("G-PKLHX0TM5S");
-  }
+  ReactGA.initialize("G-PKLHX0TM5S", {
+    testMode: !isProd,
+  });
 }
 
 export function reportPageHit() {
-  checkEnvironment();
-  if (isProd) {
-    const path = window.location.pathname;
-    if (path !== previousPath) {
-      ReactGA.send({ hitType: "pageview", page: path, title: document.title });
-      previousPath = path;
-    }
+  const path = window.location.pathname;
+  if (path !== previousPath) {
+    previousPath = path;
+    ReactGA.send({
+      hitType: "pageview",
+      page: path,
+      title: document.title,
+    });
   }
 }
