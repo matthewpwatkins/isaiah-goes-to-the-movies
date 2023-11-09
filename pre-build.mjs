65254397789c47055b164e2c77de1d254ef24f4e
Matthew Watkins <3808901+TodoCleverNameHere@users.noreply.github.com>
2023-11-09 15:07:00 -0600

    Sitemap in XML format

--- pre-build.mjs
+++ pre-build.mjs
@@ -10,11 +10,12 @@ const DST_DATA_FOLDER = path.join(PUBLIC_FOLDER, "data");
 const DST_CHAPTERS_FOLDER = path.join(DST_DATA_FOLDER, "chapters");
 const DST_CHAPTER_IMAGES_FOLDER = path.join(DST_DATA_FOLDER, "chapter-images");
 const DST_CHAPTER_LISTINGS_FILE_PATH = path.join(DST_CHAPTERS_FOLDER, "_list");
-const DST_SITEMAP_FILE_PATH = path.join(PUBLIC_FOLDER, "sitemap.txt");
+const DST_SITEMAP_TXT_FILE_PATH = path.join(PUBLIC_FOLDER, "sitemap.txt");
+const DST_SITEMAP_XML_FILE_PATH = path.join(PUBLIC_FOLDER, "sitemap.xml");
 
 // Wipe out existing pre-build assets
 await fsAsync.rm(DST_DATA_FOLDER, { recursive: true, force: true });
-await fsAsync.rm(DST_SITEMAP_FILE_PATH, { force: true });
+await fsAsync.rm(DST_SITEMAP_TXT_FILE_PATH, { force: true });
 await fsAsync.mkdir(DST_DATA_FOLDER);
 await fsAsync.mkdir(DST_CHAPTERS_FOLDER);
 await fsAsync.mkdir(DST_CHAPTER_IMAGES_FOLDER);
@@ -38,6 +39,9 @@ const downloadFile = async (url, destinationFilePath) => {
 
 const siteMap = ["https://isaiahgoestothemovies.com"];
 siteMap.push(`${siteMap[0]}/sitemap.txt`);
+siteMap.push(`${siteMap[0]}/sitemap.xml`);
+siteMap.push(`${siteMap[0]}/favicon-32x32.png`);
+
 const chapterListings = [];
 for (const fileName of await fsAsync.readdir(SRC_ENTRIES_FOLDER)) {
   const sourceFilePath = path.join(SRC_ENTRIES_FOLDER, fileName);
@@ -66,5 +70,13 @@ await fsAsync.writeFile(
 );
 console.log(`Done writing index. Size: ${chapterListings.length}`);
 
-await fsAsync.writeFile(DST_SITEMAP_FILE_PATH, siteMap.join("\n") + "\n");
-console.log("Done writing sitemap");
+await fsAsync.writeFile(DST_SITEMAP_TXT_FILE_PATH, siteMap.join("\n") + "\n");
+await fsAsync.writeFile(DST_SITEMAP_XML_FILE_PATH, `<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <url>
+    <loc>${siteMap.join(`</loc>
+  </url>
+  <url>
+    <loc>`)}</loc>
+  </url>
+</urlset>`);
+console.log("Done writing sitemaps");
